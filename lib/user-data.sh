3726e254982f35fb479a36e8edb2f4d1ad7fc246
Steve Thompson <scthomps312@gmail.com>
2021-08-24 08:52:21 -0600

    Update script to actually install NPM on ec2 creation.

--- lib/user-data.sh
+++ lib/user-data.sh
@@ -1,8 +1,23 @@
-# https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-up-node-on-ec2-instance.html
 #!/bin/bash -xe
-sudo su
 yum update -y
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+cat > /tmp/subscript.sh << EOF
+# START
+echo "Setting up NodeJS Environment"
+curl https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+
+echo 'export NVM_DIR="/home/ec2-user/.nvm"' >> /home/ec2-usr/.bashrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm' >> /home/ec2-user/.bashrc
+
+# Dot source the files to ensure that variables are available within the current shell
+. /home/ec2-user/.nvm/nvm.sh
+. /home/ec2-user/.bashrc
+
+# Install NVM, NPM, Node.JS & Grunt
+nvm alias default v16.4.0
+nvm install v16.4.0
+nvm use v16.4.0
+EOF
+
+chown ec2-user:ec2-user /tmp/subscript.sh && chmod a+x /tmp/subscript.sh
+sleep 1; su - ec2-user -c "/tmp/subscript.sh"
