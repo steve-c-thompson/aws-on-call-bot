d10fdfcb873dfd7e213353aacc0cbfcb3c5d99c6
steve-c-thompson <scthomps312@gmail.com>
2021-10-29 11:42:50 -0600

    Ecs (#2)
    * Attempt at cluster with secrets. Not sure about accessing and roles.
    
    * Getting secret from name only.
    
    * Building VPC is now a utils method.
    
    * Functionality to deploy, but Secrets are not working.
    
    * Add second buildSecret method to try fixing issues.
    
    * Working to hit ec2 IP address directly.
    
    * Working state, except we have to go around the load balancer. That's no good.

---
+++ src/aws-on-call-bot-ecs-stack.ts
@@ -0,0 +1,159 @@
+import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import {
+  AwsLogDriverMode,
+  Ec2TaskDefinition,
+  NetworkMode,
+} from "@aws-cdk/aws-ecs";
+import * as elb from "@aws-cdk/aws-elasticloadbalancing";
+import { LoadBalancingProtocol } from "@aws-cdk/aws-elasticloadbalancing";
+import * as path from "path";
+import * as ecrdeploy from "cdk-ecr-deployment";
+
+import { AddCapacityOptions } from "@aws-cdk/aws-ecs/lib/cluster";
+import { KeyPair } from "cdk-ec2-key-pair";
+import { buildPublicVpc, buildSecrets } from "./utils/utils";
+import { Repository } from "@aws-cdk/aws-ecr";
+import { DockerImageAsset } from "@aws-cdk/aws-ecr-assets";
+import { RetentionDays } from "@aws-cdk/aws-logs";
+
+export class AwsOnCallBotEcsStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create new VPC
+    const vpc = buildPublicVpc(this, "on-call-ecs-vpc");
+
+    // Open port 22 for SSH connection from anywhere
+    const securityGroup = new ec2.SecurityGroup(this, "SecurityGroup", {
+      vpc,
+      securityGroupName: "on-call-bot-ecs-sg",
+      description: "Allow ssh access to ec2 instances from anywhere",
+      allowAllOutbound: true,
+    });
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      "allow public ssh access"
+    );
+    // securityGroup.addIngressRule(
+    //   ec2.Peer.anyIpv4(),
+    //   ec2.Port.tcp(3000),
+    //   "allow HTTP traffic to port 3000 for bot node js"
+    // );
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(5000),
+      "allow HTTP traffic to port 5000 for bot node js"
+    );
+
+    const key = new KeyPair(this, "KeyPair", {
+      name: "cdk-keypair",
+      description: "Key Pair created with CDK Deployment",
+    });
+
+    // Capacity
+    const ec2Capacity: AddCapacityOptions = {
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO
+      ),
+      maxCapacity: 1,
+      minCapacity: 1,
+      // can make ec2 instances accessible by IP address when using NAT gateway
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      keyName: key.keyPairName,
+    };
+
+    const secrets = buildSecrets(this);
+
+    const repository = new Repository(this, "OnCallBotRepo", {
+      imageScanOnPush: true,
+    });
+
+    // Docker image
+    const imageName = "on-call-bot";
+    const asset = new DockerImageAsset(this, "CDKDockerImage", {
+      directory: path.relative(__dirname, imageName),
+    });
+
+    new ecrdeploy.ECRDeployment(this, "DeployDockerImage", {
+      src: new ecrdeploy.DockerImageName(asset.imageUri),
+      dest: new ecrdeploy.DockerImageName(
+        // `${cdk.Aws.ACCOUNT_ID}.dkr.ecr.${cdk.Aws.REGION}.amazonaws.com/${imageName}`
+        repository.repositoryUri
+      ),
+    });
+
+    const ec2Task: Ec2TaskDefinition = new Ec2TaskDefinition(this, "ec2-task", {
+      //networkMode: NetworkMode.AWS_VPC,
+    });
+
+    const container = ec2Task.addContainer("defaultContainer", {
+      // fromDockerImageAsset does not work with ECRDeployment
+      // image: ecs.ContainerImage.fromDockerImageAsset(asset),
+      // image: ecs.EcrImage.fromDockerImageAsset(asset),
+      // image: ecs.RepositoryImage.fromDockerImageAsset(asset),
+
+      // Works when repository used
+      image: ecs.ContainerImage.fromEcrRepository(repository),
+
+      memoryLimitMiB: 256,
+      secrets: secrets,
+      portMappings: [
+        // { containerPort: 3000, hostPort: 3000 },
+        { containerPort: 5000, hostPort: 5000 },
+      ],
+      logging: new ecs.AwsLogDriver({
+        streamPrefix: "OnCallBotStr",
+        mode: AwsLogDriverMode.NON_BLOCKING,
+        logRetention: RetentionDays.ONE_DAY,
+      }),
+    });
+
+    // Create an ECS cluster
+    // Adding Capacity with deprecated method because cluster.connections.addSecurityGroup does not work
+    const cluster = new ecs.Cluster(this, "BotCluster", {
+      vpc: vpc,
+      //capacity: ec2Capacity,
+    });
+    //cluster.connections.addSecurityGroup(securityGroup);
+
+    // Adding security group allows ec2 instance to be access from internet
+    cluster
+      .addCapacity("BotClusterCapacity", ec2Capacity)
+      .addSecurityGroup(securityGroup);
+
+    // Instantiate an Amazon ECS Service
+    const ecsService = new ecs.Ec2Service(this, "BotService", {
+      cluster: cluster,
+      taskDefinition: ec2Task,
+      // available for awsvpc network mode
+      //securityGroups: [securityGroup],
+    });
+
+    // Load balancer
+    const lb = new elb.LoadBalancer(this, "LB", { vpc });
+    lb.addListener({
+      externalPort: 5000,
+      externalProtocol: LoadBalancingProtocol.TCP,
+    });
+    lb.addTarget(ecsService);
+
+    // Create outputs for connecting
+    new cdk.CfnOutput(this, "Cluster ARN", { value: cluster.clusterArn });
+    new cdk.CfnOutput(this, "Key Name", { value: key.keyPairName });
+    new cdk.CfnOutput(this, "Download Key Command", {
+      value:
+        "aws [--profile username] secretsmanager get-secret-value --secret-id ec2-ssh-key/cdk-keypair/private --query SecretString --output text > cdk-key.pem && chmod 400 cdk-key.pem",
+    });
+    new cdk.CfnOutput(this, "ssh command", {
+      value:
+        "ssh -i cdk-key.pem -o IdentitiesOnly=yes ec2-user@" +
+        "   ...find IP at https://console.aws.amazon.com/ecs/",
+    });
+  }
+}
---
+++ src/utils/utils.ts
@@ -0,0 +1,148 @@
+import { Secret, Secret as EcsSecret } from "@aws-cdk/aws-ecs";
+import { Secret as SmSecret, ISecret } from "@aws-cdk/aws-secretsmanager";
+import { Construct, Stack } from "@aws-cdk/core";
+import { Vpc } from "@aws-cdk/aws-ec2";
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as iam from "@aws-cdk/aws-iam";
+
+/** Creates a Secret Manager secret that we directly consume in ECS. */
+export function newEcsSecret2(stack: Stack, name: string): EcsSecret {
+  return EcsSecret.fromSecretsManager(
+    new SmSecret(stack, name, { secretName: name })
+  );
+}
+export function buildSecrets2(stack: Stack) {
+  const secrets: Record<string, Secret> = {
+    SLACK_SIGNING_SECRET: newEcsSecret2(stack, awsInfo.getSecretName()),
+    SLACK_BOT_TOKEN: newEcsSecret2(stack, awsInfo.getSecretName()),
+    GOOGLE_SERVICE_ACCOUNT_EMAIL: newEcsSecret2(stack, awsInfo.getSecretName()),
+    GOOGLE_PRIVATE_KEY: newEcsSecret2(stack, awsInfo.getSecretName()),
+    SLACK_GOOGLE_SHEET_ID: newEcsSecret2(stack, awsInfo.getSecretName()),
+  };
+  return secrets;
+}
+
+export function newEcsSecret(secret: ISecret, name: string): EcsSecret {
+  return EcsSecret.fromSecretsManager(secret, name);
+}
+
+export function smSecretFromName(
+  scope: Construct,
+  id: string,
+  secretName: string
+): ISecret {
+  return SmSecret.fromSecretNameV2(scope, id, secretName);
+}
+
+export function buildPublicVpc(scope: Construct, name: string): Vpc {
+  return new ec2.Vpc(scope, name, {
+    natGateways: 1,
+    maxAzs: 1,
+    // subnetConfiguration: [
+    //   {
+    //     cidrMask: 24,
+    //     name: "public",
+    //     subnetType: ec2.SubnetType.PUBLIC,
+    //   },
+    // ],
+    // enableDnsSupport: true,
+    // enableDnsHostnames: true,
+  });
+}
+
+// export function buildSecrets2(scope: Construct) {
+//   let secretStr, decodedBinarySecret;
+//   const client = new SecretsManager({
+//     region: awsInfo.getRegion(),
+//   });
+//   client.getSecretValue(
+//     { SecretId: awsInfo.getSecretName() },
+//     function (err, data) {
+//       if (err) {
+//         if (err.code === "DecryptionFailureException")
+//           // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+//           // Deal with the exception here, and/or rethrow at your discretion.
+//           throw err;
+//         else if (err.code === "InternalServiceErrorException")
+//           // An error occurred on the server side.
+//           // Deal with the exception here, and/or rethrow at your discretion.
+//           throw err;
+//         else if (err.code === "InvalidParameterException")
+//           // You provided an invalid value for a parameter.
+//           // Deal with the exception here, and/or rethrow at your discretion.
+//           throw err;
+//         else if (err.code === "InvalidRequestException")
+//           // You provided a parameter value that is not valid for the current state of the resource.
+//           // Deal with the exception here, and/or rethrow at your discretion.
+//           throw err;
+//         else if (err.code === "ResourceNotFoundException")
+//           // We can't find the resource that you asked for.
+//           // Deal with the exception here, and/or rethrow at your discretion.
+//           throw err;
+//       } else {
+//         // Decrypts secret using the associated KMS CMK.
+//         // Depending on whether the secret is a string or binary, one of these fields will be populated.
+//         if ("SecretString" in data) {
+//           secretStr = data.SecretString;
+//         } else {
+//           let buff = new Buffer(data.SecretBinary as string, "base64");
+//           secretStr = buff.toString("ascii");
+//         }
+//       }
+//
+//       // Your code goes here.
+//     }
+//   );
+// }
+export function buildSecrets(scope: Construct) {
+  const secret = smSecretFromName(
+    scope,
+    "oncall-bot-secret",
+    awsInfo.getSecretName()
+  );
+
+  const role = new iam.Role(scope, "OncallSecretReader", {
+    assumedBy: new iam.AccountRootPrincipal(),
+  });
+  secret.grantRead(role);
+
+  const secrets: Record<string, Secret> = {
+    SLACK_SIGNING_SECRET: newEcsSecret(secret, "SLACK_SIGNING_SECRET"),
+    SLACK_BOT_TOKEN: newEcsSecret(secret, "SLACK_BOT_TOKEN"),
+    GOOGLE_SERVICE_ACCOUNT_EMAIL: newEcsSecret(
+      secret,
+      "GOOGLE_SERVICE_ACCOUNT_EMAIL"
+    ),
+    GOOGLE_PRIVATE_KEY: newEcsSecret(secret, "GOOGLE_PRIVATE_KEY"),
+    SLACK_GOOGLE_SHEET_ID: newEcsSecret(secret, "SLACK_GOOGLE_SHEET_ID"),
+  };
+  return secrets;
+}
+
+class AwsInfo {
+  //arn:aws:secretsmanager:us-east-2:146543024844:secret:OncallSlackBot-test-NMgAjb
+  getAccountNumber(): string {
+    return process.env.CDK_DEFAULT_ACCOUNT || "146543024844";
+  }
+
+  getRegion(): string {
+    return process.env.CDK_DEFAULT_REGION || "us-east-2";
+  }
+
+  getSlackBotSecretArn(): string {
+    return (
+      "arn:aws:secretsmanager:" +
+      this.getRegion() +
+      ":" +
+      this.getAccountNumber() +
+      ":secret:" +
+      this.getSecretName()
+    );
+  }
+
+  getSecretName(): string {
+    return "OncallSlackBot-prod";
+  }
+}
+
+export const awsInfo = new AwsInfo();
